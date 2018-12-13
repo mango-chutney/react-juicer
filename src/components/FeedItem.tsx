ceb6589a57745202686feef33451644327b63468
Francisco Martinez <francis@mangochutney.com.au>
2018-12-13 12:23:37 +1030

    fix: feed hover

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -1,6 +1,34 @@
 import styled, { css } from 'styled-components';
 import * as styles from '../styles';
-import { FeedItemHover } from './index';
+
+export const FeedItemHover = styled.div`
+  align-items: center;
+  background: #00000055;
+  color: #ffffff;
+  display: none;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+
+  i {
+    margin-right: 0.25rem;
+  }
+
+  > div {
+    align-items: center;
+    display: inline-flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    margin-right: 0.5rem;
+  }
+`;
+
+FeedItemHover.defaultProps = {
+  theme: styles.defaultTheme,
+};
 
 const FeedItem = styled.div`
   display: flex;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,9 @@
 export { ChevronLeft, ChevronRight, Comments, Cross, Heart } from './Icons';
 export { default as AnchorButton } from './AnchorButton';
 export { default as Button } from './Button';
-export { default as FeedItem } from './FeedItem';
+export { default as FeedItem, FeedItemHover } from './FeedItem';
 export { default as FeedItemImageContainer } from './FeedItemImageContainer';
 export { default as FeedItemBackgroundImage } from './FeedItemBackgroundImage';
-export { default as FeedItemHover } from './FeedItemHover';
 export { default as FeedWrapper } from './FeedWrapper';
 export { default as Juicer } from './Juicer';
 export { default as LoadMoreContainer } from './LoadMoreContainer';
